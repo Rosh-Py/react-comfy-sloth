9f79fb75e4bb96ba1b0c2fe99f93bac4a894ae52
Rosh-Py <jharoshan.k54@gmail.com>
2021-06-18 13:12:43 +0530

    added sort functionality

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -5,7 +5,7 @@ const ProductImages = ({ images = [{ url: "" }] }) => {
   const [main, setMain] = useState(images[0]);
   return (
     <Wrapper>
-      <img src={main.url} alt="main image" />
+      <img src={main.url} className="main" alt="main image" />
       <div className="gallery">
         {images.map((image, index) => {
           return (
